7ed092917a2dc018d71bc5b932626df91254f235
Yaokai Liu <yaokai.liu@foxmail.com>
2024-09-08 12:02:48 +0800

    update grammar.

--- demo.mm
+++ demo.mm
@@ -1,10 +1,10 @@
 machine x64 {
   register ax [64-bits] {
-    r~ : [63-0] = 0;
-    e~ : [31-0] = 1;
-    ~  : [15-0] = 2;
-    ah : [15-8] = 4;
-    al : [7-0] = 8;
+    r~ : [63-0] = 0x00;
+    e~ : [31-0] = 0x00;
+    ~  : [15-0] = 0x00;
+    ah : [15-8] = 0x04;
+    al : [7-0]  = 0x00;
   };
   immediate refer [23-bit] unsigned;
   memory local [12-bit] {
@@ -12,13 +12,13 @@ machine x64 {
     $: [0-5];
   };
   instruction mov {
-    [ax, local] = [12-byte] (4-tick)  {
+    [ax, local] = [10-byte] (4-tick) {
       ^ : [8] = 0x56;
       & : [8] = 0x37;
       ~ : [64] = {
-        [0-5] = local.base,
-        [63-56] = $ax,
-        [22-11] = local.offset,
+        [0-5] = local.$,
+        [63-56] = ax,
+        [22-11] = local.>,
         [...] = 0
       };
     };
