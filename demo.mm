e9544b54888d191b4c4ab1ae10a3f29834b23e2c
Yaokai Liu <yaokai.liu@foxmail.com>
2024-11-06 14:42:45 +0800

    update 'demo.mm'.

--- demo.mm
+++ demo.mm
@@ -8,18 +8,18 @@ machine x64 {
   };
   immediate refer [23-bit] unsigned;
   memory local [12-bit] {
-    >: [6-11];
-    $: [0-5];
+    $: [6-11];
+    >: [0-5];
   };
   instruction mov {
     [gax, local] = [10-byte] (4-tick) {
       ^ : [8] = 0x56;
       & : [8] = 0x37;
       ~ : [64] = {
-            [0-5] = local.$,
-            [63-56] = gax,
-            [22-11] = local.>,
-            [...] = 0
+        [0-5] = local.$,
+        [63-56] = gax,
+        [22-11] = local.>,
+        [...] = 0
       };
     };
   };
