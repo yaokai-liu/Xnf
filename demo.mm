f5a352f578d504361e3fbb099180cb50e3522997
Yaokai Liu <yaokai.liu@foxmail.com>
2025-01-23 17:06:40 +0800

    update demo.

--- demo.mm
+++ demo.mm
@@ -6,6 +6,13 @@ machine x64 {
     ah : [15-8] = 0x04;
     al : [7-0]  = 0x00;
   };
+  register gbx [64-bit] {
+    rbx: [63-0] = 0x00;
+    ebx: [31-0] = 0x00;
+    bx : [15-0] = 0x00;
+    bh : [15-8] = 0x04;
+    bl : [7-0]  = 0x00;
+  };
   immediate refer [23-bit] unsigned;
   memory local [12-bit] {
     $: [6-11];
@@ -15,8 +22,9 @@ machine x64 {
     $: [13-24];
     >: [0-12];
   };
+  set gpr {gax, gbx};
   instruction mov {
-    [gax, local] = [10-byte] (4-tick) {
+    [gpr, local] = [10-byte] (4-tick) {
       ^ : [8] = 0x56;
       & : [8] = 0x37;
       ~ : [64] = {
