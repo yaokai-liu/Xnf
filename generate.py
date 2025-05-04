608ca0347ae1e5c3722ce15bbe318ef1acef778e
Yaokai Liu <yaokai.liu@foxmail.com>
2025-05-04 08:38:14 +0800

    update grammar.

--- generate.py
+++ generate.py
@@ -18,3 +18,4 @@ if __name__ == '__main__':
     with open(__dir__ / 'regex.xnf', 'r', encoding='utf-8') as f:
         XNF_PARSER.set_rules(f.read(), start='Regexp')
     XNF_PARSER.dump(cfd, compact=compact)
+
