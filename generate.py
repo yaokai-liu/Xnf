caf33db1fcb99bb25bb17d25df48190accb5f301
Yaokai Liu <yaokai.liu@foxmail.com>
2025-05-30 10:34:54 +0800

    set grammar environments.

--- generate.py
+++ generate.py
@@ -4,6 +4,13 @@ from pathlib import Path
 from xnf import *
 __dir__ = Path(os.path.dirname(__file__))
 
+ENVS = {
+    "$",
+    "GrammarEntry",
+    "RIGHT_BRACKET",
+    "SEMICOLON"
+}
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         cfd = __dir__
@@ -17,5 +24,6 @@ if __name__ == '__main__':
         compact = True
     with open(__dir__ / 'xLR.xnf', 'r', encoding='utf-8') as f:
         XNF_PARSER.set_rules(f.read(), start='GrammarEntry')
+    XNF_PARSER.add_environment(ENVS)
     XNF_PARSER.dump(cfd / 'xLR', compact=compact)
 
