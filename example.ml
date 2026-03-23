4f705eb5040f0a92b9175cf9a937585d8823cf7d
Yaokai Liu <yaokai.liu@foxmail.com>
2026-03-23 15:44:21 +0800

    update grammar; add example

--- example.ml
+++ example.ml
@@ -1,9 +1,9 @@
 Definition (limitation of series) {
     limitation of a series{a(n)} is a real number{a} that $\forall \epsilon \in R > 0$,
     $\exists N \in \mathbb{N} > 0$ that $\forall n > N$ there is $|a(n) - a| < \epsilon$.
-} Denoteas { [limitation of series] (series{a(n)}) <$ \lim_{#{n}\to\infty}#{a(n)} $>; }
+} Denoteas { [limitation of series] (series{a(n)}) $ \lim_{#{n}\to\infty}#{a(n)} $; }
 
 Definition (limitation of function) {
     limitation of a function{f(x)} at $x_0 \in R$ is a real number{a} that $\forall \epsilon \in R > 0$,
     $\exists \delta \in R : \delta > 0$ that $\forall x : |x - x_0| < \delta$ there is $|f(x) - a| < \epsilon$.
-} Denoteas { [limitation of function] (function{f(x)}, target{x_0}) <$ \lim_{#{x}\to#{x_0}}#{f(x)} $>; }
+} Denoteas { [limitation of function] (function{f(x)}, target{x_0}) $ \lim_{#{x}\to#{x_0}}#{f(x)} $; }
