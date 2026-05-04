4874c22af772f21a0e3ccdfe9911a913ab5ac61b
Yaokai Liu <yaokai.liu@foxmail.com>
2026-05-04 23:14:05 +0800

    update grammar.

--- example.ml
+++ example.ml
@@ -1,11 +1,11 @@
-Definition (limit@Noun of sequence) {
+Definition (limit of sequence) {
     limit of sequence{a(n)} is a real number{a} that $\forall \epsilon \in R > 0$,
     $\exists N \in \mathbb{N} > 0$ that $\forall n > N$ there is $|a(n) - a| < \epsilon$.
 } denote {
     [limit of sequence] (sequence{a(n)}) as $ \lim_{#{n}\to\infty}#{a(n)} $;
 }
 
-Definition (limit@Noun of sequence) {
+Definition (limit of sequence) {
     limit of function{f(x)} at $x_0 \in R$ is one real number{a} that $\forall \epsilon \in R > 0$,
     $\exists \delta \in R : \delta > 0$ that $\forall x : |x - x_0| < \delta$ there is $|f(x) - a| < \epsilon$.
 } denote {
